528ea6f112397d678c83bf6fd44589d42e4f6a62
Darren Jensen <darren.jensen@gmail.com>
2018-02-03 11:19:39 +0700

    Added full CRUD functionality

---
+++ README.md
@@ -0,0 +1,7 @@
+To run the app using docker-compose:
+
+`docker-compose up`
+
+To shell into the *running container*:
+
+`docker exec -it ideaboard-api bash`
--- config/application.rb
+++ config/application.rb
@@ -34,8 +34,8 @@ module IdeaboardApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        origins 'http://localhost:3000'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
   end
